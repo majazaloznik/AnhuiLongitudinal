88de7aa068c9fc5a47e4e3d90c93aa4e8a7593c7
majazaloznik <maja.zaloznik@gmail.com>
2016-11-04 18:39:04 +0000

    added huijun's table on anhui and all my misunderstandings of it in lit.rmd

--- scripts/00-testing.R
+++ scripts/00-testing.R
@@ -5,3 +5,6 @@ test <- read.spss("data/archives/ICPSR_36388-2001/ICPSR_36388/DS0001/36388-0001-
 test2 <- read.dta("data/archives/ICPSR_36388-2001/ICPSR_36388/DS0001/36388-0001-Data.dta")
 
 test2003 <- read.dta("data/archives/ICPSR_36388-2003/ICPSR_36388/DS0002/36388-0002-Data.dta")
+
+test.1 <- read.dta("data/charls/CHARLS_Life_History_Data/Sample_Infor_12.dta")
+test.1 <- read.dta("data/charls/CHARLS_Life_History_Data/Work_History_12.dta")
